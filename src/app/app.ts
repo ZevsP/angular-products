351369f06a986f06550da3af34c64f1eb1cd09c1
ZevsP <sermarser31@gmail.com>
2025-10-20 17:36:04 +0200

    v1.1.0 create products list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Hello} from './hello/hello';  
+import {Hello} from './hello/hello';
+import { ProductList } from './product-list/product-list'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hello],
+  imports: [RouterOutlet, Hello, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/product-list/product-list.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Product } from '../interfaces/product';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [MatCardModule],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css'
+})
+export class ProductList {
+  title = 'Mi lista de productos';
+  headers = {
+    description: 'Producto',
+    price: 'Precio',
+    available: 'Disponible'
+  }
+
+   products: Product[] = [
+    {
+      id: 1,
+      description: 'SSD hard drive',
+      available: '2024-10-03',
+      price: 75,
+      imageUrl: 'assets/ssd.jpg',
+      rating: 5
+    }, {
+      id: 2,
+      description: 'LGA1151 Motherboard',
+      available: '2024-09-15',
+      price: 96.95,
+      imageUrl: 'assets/motherboard.jpg',
+      rating: 4
+    }
+  ];
+}
