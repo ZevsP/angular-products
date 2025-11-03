13206a6cce3733caae270a60daf25d88f408a499
ZevsP <sermarser31@gmail.com>
2025-11-03 18:53:40 +0100

    v3.0.1 config app

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -1,37 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
 import { Product } from '../interfaces/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  productsEndpoint = 'http://localhost:3000/products';
 
-  getProducts(): Product[] {
-    return [
-      {
-        id: 1,
-        description: 'SSD hard drive',
-        available: '2024-10-03',
-        price: 75,
-        imageUrl: 'assets/ssd.jpg',
-        rating: 5
-      }, {
-        id: 2,
-        description: 'LGA1151 Motherboard',
-        available: '2024-09-15',
-        price: 96.95,
-        imageUrl: 'assets/motherboard.png',
-        rating: 4
-      },
-      {
-        id: 3,
-        description: 'Kingston DDR4 3200 MHz 16GB 2x8GB CL16',
-        available: '2024-11-10',
-        price: 42.95,
-        imageUrl: 'assets/ssd.jpg',
-        rating: 4
-      }
-    ];
+  private http = inject(HttpClient);
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsEndpoint);
   }
-  
+
 }
